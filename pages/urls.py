90537401524eea12d6ff5fd04cb025c375a9ad01
pipeagudelo3 <125285931+pipeagudelo3@users.noreply.github.com>
2025-08-06 08:06:53 -0500

    Subiendo cambios del carrio

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     HomePageView, AboutPageView,
-    ProductIndexView, ProductShowView, ProductCreateView
+    ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
 )
 
 urlpatterns = [
@@ -11,5 +11,9 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='product-created'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('products/create/', ProductCreateView.as_view(), name='create'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
 ]
 
